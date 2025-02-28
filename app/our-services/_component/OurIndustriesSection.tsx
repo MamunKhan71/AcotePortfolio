f25532a22a1df929002a5b91ac010eb1baab6753
Md. Mamun <mkmamun031@gmail.com>
2025-02-28 14:20:23 +0600

    :bulb:Completed Our Industries Section

--- app/our-services/_component/OurIndustriesSection.tsx
+++ app/our-services/_component/OurIndustriesSection.tsx
@@ -1,13 +1,14 @@
 import ComponentTitle from '@/components/common/ComponentTitle'
-import FinanceIcon from '@/components/icons/healthcare-icon'
-import React from 'react'
+import categoriesData, { CategoryDataTypes } from '@/data/our-industries-data'
 
 const OurIndustriesSection = () => {
     return (
         <div>
             <ComponentTitle title='Our industries' />
-            <div className='grid grid-cols-3 gap-[18px]'>
-                <OurIndustriesCard />
+            <div className='grid grid-cols-3 gap-[18px] mt-10'>
+                {
+                    categoriesData?.map((category: CategoryDataTypes) => <OurIndustriesCard key={category.id} icon={category.icon} name={category.name} />)
+                }
             </div>
         </div>
     )
@@ -16,12 +17,12 @@ const OurIndustriesSection = () => {
 export default OurIndustriesSection
 
 
-const OurIndustriesCard = () => {
+const OurIndustriesCard = ({ icon, name }: CategoryDataTypes) => {
     return (
         <div className='relative bg-[#E2E2E2]/50 px-4 py-6 rounded-sm'>
-            <p className='text-xl font-medium'>Finance</p>
-            <div className='absolute right-8 bottom-0'>
-                <FinanceIcon />
+            <p className='text-xl font-medium'>{name}</p>
+            <div className='absolute right-16 bottom-0'>
+                {icon}
             </div>
         </div>
     )
---
+++ data/our-industries-data.tsx
@@ -0,0 +1,28 @@
+import AiIcon from "@/components/icons/ai-icon";
+import AutoMotive from "@/components/icons/automotive-icon";
+import ConstructionIcon from "@/components/icons/construction-icon";
+import EcommerceIcon from "@/components/icons/ecommrece-icon";
+import EducationIcon from "@/components/icons/education-icon";
+import FinanceIcon from "@/components/icons/finance-icon";
+import HealthcareIcon from "@/components/icons/healthcare-icon";
+import TravelIcon from "@/components/icons/travel-icon";
+
+export interface CategoryDataTypes {
+    id?: number;
+    name: string;
+    icon: React.ReactNode;
+};
+
+const categoriesData: CategoryDataTypes[] = [
+    { id: 1, name: "Finance", icon: <FinanceIcon /> },
+    { id: 4, name: "Healthcare", icon: <HealthcareIcon /> },
+    { id: 7, name: "Education", icon: <EducationIcon /> },
+    { id: 2, name: "Artificial Intelligence", icon: <AiIcon /> },
+    { id: 5, name: "Construction", icon: <ConstructionIcon /> },
+    { id: 8, name: "Ecommerce", icon: <EcommerceIcon /> },
+    { id: 3, name: "Automotive", icon: <AutoMotive /> },
+    { id: 6, name: "Travel", icon: <TravelIcon /> },
+    { id: 9, name: "Others", icon: <EcommerceIcon /> },
+];
+
+export default categoriesData;
