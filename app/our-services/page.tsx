0873c306f605359a301b7bfb334cd9106c7b05e7
Md. Mamun <mkmamun031@gmail.com>
2025-02-28 14:39:46 +0600

    :bulb:Added Contact us section

--- app/our-services/page.tsx
+++ app/our-services/page.tsx
@@ -1,3 +1,4 @@
+import ContactUsBanner from '@/components/common/contact-us-banner'
 import OurProjects from '../(homepage)/our-projects'
 import FAQSection from './_component/FAQSection'
 import OurIndustriesSection from './_component/OurIndustriesSection'
@@ -7,15 +8,18 @@ import TechnologiesSection from './_component/TechnologiesSection'
 
 const OurServices = () => {
     return (
-        <div className='container mx-auto w-full mt-10'>
-            <OurServicesHeader />
-            <div className='space-y-24 mb-24'>
-                <OurServicesApproach />
-                <TechnologiesSection />
-                <OurIndustriesSection />
-                <OurProjects isService={true} />
-                <FAQSection />
+        <div>
+            <div className='container mx-auto w-full mt-10'>
+                <OurServicesHeader />
+                <div className='space-y-24 mb-24'>
+                    <OurServicesApproach />
+                    <TechnologiesSection />
+                    <OurIndustriesSection />
+                    <OurProjects isService={true} />
+                    <FAQSection />
+                </div>
             </div>
+            <ContactUsBanner />
         </div>
     )
 }
